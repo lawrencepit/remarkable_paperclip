5917a3569a948d0eee1fd423212dcc80b5d09ad2
Lawrence Pit <lawrence.pit@gmail.com>
2009-05-25 21:55:31 +1000

    Remarkable.include_matchers! change.

--- lib/remarkable_paperclip.rb
+++ lib/remarkable_paperclip.rb
@@ -3,5 +3,10 @@ require 'validate_attachment_size_matcher'
 require 'validate_attachment_presence_matcher'
 require 'validate_attachment_content_type_matcher'
 
-Remarkable.include_matchers!(Remarkable::Paperclip, Spec::Example::ExampleGroup)
+if defined?(Spec::Rails)
+  Remarkable.include_matchers!(Remarkable::Paperclip, Spec::Rails::Example::ModelExampleGroup)
+else
+  Remarkable.include_matchers!(Remarkable::Paperclip, Spec::Example::ExampleGroup)
+end
+
 Remarkable.add_locale(File.dirname(__FILE__) + '/en.yml')
